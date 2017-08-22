f06b31e64c634d8f22ffeff3f78f6e9f44443954
russpoush <russpoush@gmail.com>
2017-08-22 15:25:47 -0500

    Update CernerTweets.r
    took out keys and tokens

--- CernerTweets.r
+++ CernerTweets.r
@@ -2,10 +2,10 @@ if (!require("twitteR")) {
   install.packages("twitteR", repos="http://cran.rstudio.com/") 
   library("twitteR")
 }
-consumer_key <- "39icLunl5greGGOXEHNHcZXcf"
-consumer_secret <- "ey1duDRIykb6E3Ev72qKDMp4QTnj6ODuRjie5fdk1ugNy7mtk9"
-access_token <- "70570157-FT2piNK7WQPmplRSLcDJ9UFBmKLXSzu1RhcHcBV0K"
-access_secret <- "JqHjhynaTIDv7EsLIXBE3S8XS75bu080sm2ejgxa4F3tm"
+consumer_key <- "############"
+consumer_secret <- "############"
+access_token <- "############"
+access_secret <- "############"
 options(httr_oauth_cache=T) #This will enable the use of a local file to cache OAuth access credentials between R sessions.
 setup_twitter_oauth(consumer_key,
                     consumer_secret,
